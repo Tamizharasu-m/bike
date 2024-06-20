af78171a5b87544837edc5255de69899e6febe25
Tamizharasu-m <d.mtamizharasu25@gmail.com>
2024-06-20 16:41:42 +0530

    First commit

--- src/app/Component/Section2.jsx
+++ src/app/Component/Section2.jsx
@@ -6,8 +6,12 @@ import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
+
+  
+
 export default function Section2() {
     const textRef = useRef(null);
+    
 
     useEffect(() => {
         gsap.fromTo(
@@ -28,12 +32,13 @@ export default function Section2() {
         );
       }, []);
 
+     
 
     return (
         <div className='container mx-auto h-[100vh] flex items-center'>
             <div className=' max-w-xs mx-auto sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-7xl w-[100%] md:flex md:items-center'>
                 <div className=' px-[10px] text-center md:w-[50%] md:px-[20px]'>
-                    <h4 ref={textRef} className='font-mono text-2xl font-bold pb-[10px]'>Royal Enfield</h4>
+                    <h4 ref={textRef} className='font-mono text-2xl font-bold pb-[10px] md:text-4xl md:text-start'>Royal Enfield</h4>
                     <ul>
                         <li className='text-justify pb-[10px]'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
                         <li className='text-justify pb-[10px]'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
