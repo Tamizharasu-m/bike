867d00f75237f42b3b34bbd4050e128cc9e60d58
Tamizharasu-m <d.mtamizharasu25@gmail.com>
2024-06-20 12:20:15 +0530

    First commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@
 
 import Section from "./Component/Section";
 import Section1 from "./Component/Section1";
+import Section2 from "./Component/Section2";
 
 export default function Home() {
   return (
     <div>
       <Section/>
+      <Section2/>
       <Section1/>
     </div>
   );
